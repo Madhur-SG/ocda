81943bc20d15568361b914bf953c326a74ebb178
Madhur-SG <98654892+Madhur-SG@users.noreply.github.com>
2022-09-06 14:06:21 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,35 +1,18 @@
-          				#Install-Module AzureAD
-					#Connect-AzureAD
-					$aadApplication = New-AzureADApplication -DisplayName "ocda-app"
-
-					# $currentUser = (Get-AzureADUser -ObjectId (Get-AzureADCurrentSessionInfo).Account.Id)
-					# Add-AzureADApplicationOwner -ObjectId $aadApplication.ObjectId -RefObjectId $currentUser.ObjectId
-
+          				$aadApplication = New-AzureADApplication -DisplayName "ocda-app"
 					$appObjectId=$aadApplication.ObjectId
 					$appPassword = New-AzureADApplicationPasswordCredential -ObjectId $appObjectId -CustomKeyIdentifier "ClientSecret" -EndDate (Get-Date).AddYears(2)
-
-					#Provide Application (client) Id
 					$appId=$aadApplication.AppId
 					$servicePrincipal = New-AzureADServicePrincipal -AppId $appId -Tags @("WindowsAzureActiveDirectoryIntegratedApp")
-
 					$appObjectId=$aadApplication.ObjectId
 					$requiredResourcesAccess=(Get-AzureADApplication -ObjectId $appObjectId).RequiredResourceAccess
-
-					#Provide Application (client) Id
 					$appId=$aadApplication.AppId
 					$servicePrincipal = Get-AzureADServicePrincipal -All $true | Where-Object {$_.AppId -eq $appId}
-
-
-
-					#Get or provide your Office 365 Tenant Id or Tenant Domain Name
 					$tenantId = (Get-AzureADTenantDetail).ObjectId
 					$tenantIdSecret = ConvertTo-SecureString -String $tenantId -AsPlainText -Force
-					
-					#Provide Application (client) Id of your app
 					$appClientId=$aadApplication.AppId
 					$AppIdSecret = ConvertTo-SecureString -String $appClientId -AsPlainText -Force
 
-					#Provide Application client secret key
+					
 					$clientSecret = $appPassword.Value
 					$clientSecretSecret = ConvertTo-SecureString -String $clientSecret -AsPlainText -Force
 
